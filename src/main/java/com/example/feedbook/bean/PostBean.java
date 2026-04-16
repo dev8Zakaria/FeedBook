fa6cc84eeed1be97b5d4dc1f5e48245b172810d0
Atae <ahmadoun.atae@etu.uae.ac.ma>
2026-04-16 09:57:57 +0100

    Switch @RequestScoped to @ViewScoped

--- src/main/java/com/example/feedbook/bean/PostBean.java
+++ src/main/java/com/example/feedbook/bean/PostBean.java
@@ -10,14 +10,16 @@ import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.faces.application.FacesMessage;
 import jakarta.faces.context.FacesContext;
+import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Named
-@RequestScoped
-public class PostBean {
+@ViewScoped
+public class PostBean implements Serializable {
 
     @Inject
     private PostService postService;
